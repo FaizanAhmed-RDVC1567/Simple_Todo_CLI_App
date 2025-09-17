166068f4b55adcdb1228fd5df164f82f71dd8c56
FaizanAhmed-RDVC1567 <72260047+FaizanAhmed-RDVC1567@users.noreply.github.com>
2025-09-17 15:05:03 +0400

    Updated with two private functions, a 'require' statement that deal with JSON stuff.

--- lib/todo.rb
+++ lib/todo.rb
@@ -1,16 +1,23 @@
+require "json"
+
 class Todo
   attr_reader :tasks
+  FILE_PATH = "todo_data.json"
 
   def initialize
-    @tasks = []
+    @tasks = load_tasks  # methods that don't have arguments are usually called like this in Ruby
   end
 
   def add(task)
     @tasks << { task: task, done: false }
+    save_tasks
   end
 
   def complete(index)
-    @tasks[index][:done] = true if @tasks[index]
+    if @tasks[index]
+      @tasks[index][:done] = true
+      save_tasks
+    end
   end
 
   def list
@@ -18,5 +25,20 @@ class Todo
       status = t[:done] ? "[x]" : "[ ]"
       "#{i+1}. #{status} #{t[:task]}"
     end.join("\n")
+  end
+
+  private
+
+  def save_tasks
+    File.write(FILE_PATH, JSON.pretty_generate(@tasks))
+    # 'pretty_generate' is a method defined in the 'json' gem.
+  end
+
+  def load_tasks
+    if File.exist?(FILE_PATH)
+      JSON.parse(File.read(FILE_PATH), symbolize_names: true)
+    else
+      ["a"]
+    end
   end
 end
