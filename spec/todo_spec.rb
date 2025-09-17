787e28b9dff330c18b7218ece3e9ffb036b934a6
FaizanAhmed-RDVC1567 <72260047+FaizanAhmed-RDVC1567@users.noreply.github.com>
2025-09-17 14:50:33 +0400

    Updated with some tests for the Todo class.

--- spec/todo_spec.rb
+++ spec/todo_spec.rb
@@ -1,1 +1,37 @@
 # Will include some code here later. Initial commit.
+require "rspec"
+require_relative "../lib/todo.rb"
+
+RSpec.describe Todo do
+  let(:todo) {Todo.new}
+
+  # The next four lines are a setup function, like how such functions can be set in Java, JavaScript, Python, etc.
+  before do
+    # Ensure we start fresh each test
+    File.delete(Todo::FILE_PATH) if File.exist?(Todo::FILE_PATH)
+  end
+
+  it "starts with empty task list" do
+    expect(todo.tasks).to be_empty
+    # The expect 'keyword' in Ruby is like the 'assert' keyword in other languages.
+  end
+
+  it "adds a new task" do
+    todo.add("Buy milk")
+    expect(todo.tasks.size).to eq(1)  # 'eq' is in-built function that allows one to check if two objects are equal in Ruby.
+    expect(todo.tasks.first[:task]).to eq("Buy milk")
+  end
+
+  it "marks a task as complete" do
+    todo.add("Do homework")
+    todo.complete(0)
+    expect(todo.tasks.first[:done]).to be true
+  end
+
+  it "saves tasks to a file" do
+    todo.add("Read book")
+    new_todo = Todo.new  # reload from file
+    expect(new_todo.tasks.size).to eq(1)
+    expect(new_todo.tasks.first[:task]).to eq("Read book")
+  end
+end
